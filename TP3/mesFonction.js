ff054dae605faf2e5d501dd23e1d6c209f825c61
Danil gdj <danilguidjou7@gmail.com>
2024-05-21 22:17:19 +0100

    TP3 daweb

--- TP3/mesFonction.js
+++ TP3/mesFonction.js
@@ -1,5 +1,44 @@
 
-let varttt;
-function inverse(){
+const write = document.querySelector("#chaine");
+const read = document.querySelector("#resultat");
 
-}
+
+write.addEventListener("keyup", () => {
+  let name = [...write.value];
+  read.value = name.reverse().join("");
+});
+
+const arobase = document.querySelector('#myInput')
+
+arobase.addEventListener("keyup", ()=>{
+
+  let chaine = [...arobase.value];
+
+  if (chaine.includes("@")){
+    arobase.classList.add("contenueCorrect")
+    arobase.classList.remove("contenueErrone")
+  }else{
+    arobase.classList.add("contenueErrone")
+    arobase.classList.remove("contenueCorrect")
+  }
+})
+
+const pwd1 = document.querySelector("#ps1");
+const pwd2 = document.querySelector("#psd2");
+const pwd = document.querySelectorAll(".pwd")
+
+pwd.forEach((element)=>{
+  element.addEventListener("keyup",()=>{
+    if(pwd1.value == pwd2.value){
+      pwd1.classList.add("contenueCorrect");
+      pwd2.classList.add("contenueCorrect");
+      pwd1.classList.remove("contenueErrone");
+      pwd2.classList.remove("contenueErrone");
+    }else{
+      pwd1.classList.remove("contenueCorrect");
+      pwd2.classList.remove("contenueCorrect");
+      pwd1.classList.add("contenueErrone");
+      pwd2.classList.add("contenueErrone");
+    }
+  })
+})
