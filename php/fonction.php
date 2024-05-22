4f587b16b12717216e869bfe56b36f07465c916b
Danil gdj <danilguidjou7@gmail.com>
2024-05-22 17:40:21 +0100

    php basic

--- php/fonction.php
+++ php/fonction.php
@@ -11,7 +11,16 @@
     <form action="#" method="post">
         <input type="number" name="num" id="num">
         <input type="text" name="phrase" id="phrase">
+        <input type="text" name="phrase1" id="phrase2">
+        <input type="text" name="phrase2" id="phrase2">
         <input type="submit" value="verifier" name="btn">
+    </form>
+    <br><br><br><br>
+    <form action="" method="get">
+        <input type="text" name="nom" id="nom" placeholder="nom">
+        <input type="text" name="prenom" id="prenom" placeholder="prenom">
+        <input type="text" name="adresse" id="adresse" placeholder="adresse">
+        <input type="submit" value="confirmer" name="tableau">
     </form>
 
     <?php
@@ -32,10 +41,34 @@
         }
     }
 
+    function ordreAlphabitique($z, $w)
+    {
+        $z = strtolower($z);
+        $w = strtolower($w);
+
+        if ($z < $w) {
+            echo $z . " " . $w;
+        } else {
+            echo $w . " " . $z;
+        }
+    }
+
     if (isset($_POST['btn'])) {
         # verifier($_POST['num']);
         verifier(intval($_POST['num']));
         split($_POST['phrase']);
+        ordreAlphabitique($_POST['phrase1'], $_POST['phrase2']);
+    }
+    if (isset($_GET['tableau'])) {
+        $tab = [$_GET['nom'], $_GET['prenom'], $_GET['adresse']];
+        echo "<table border='1'>";
+        echo "<tr> <td> nom</td> <td> prenom</td><td> adresse</td> </tr>";
+        echo "<tr>";
+        foreach ($tab as $c) {
+            echo "<td> " . $c . "</td> ";
+        }
+        echo "</tr>";
+        echo "</table>";
     }
     ?>
 
